63b023ed9657bbc8b87c90e84b3f98ace8b38ebe
Deepa pillai <deepapillai243@gmail.com>
2025-08-27 23:10:25 +0530

    Day 26 -  non overlapping intervals

---
+++ Sorting/nonOverlappingIntervals.cs
@@ -0,0 +1,23 @@
+﻿//practise gfg link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/sorting-gfg-160/problem/non-overlapping-intervals
+
+class Solution
+{
+    public int minRemoval(int[][] intervals)
+    {
+        Array.Sort(intervals, (a, b) => a[1].CompareTo(b[1]));
+        int count = 0, end = int.MinValue;
+        for (int i = 0; i < intervals.Length; i++)
+        {
+            if ((end <= intervals[i][0]))
+            {
+                end = intervals[i][1];
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
