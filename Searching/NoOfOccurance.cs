ce12f3b52c7d12878e5501a51d7fe0926bb01bea
Deepa pillai <deepapillai243@gmail.com>
2025-10-31 22:31:41 +0530

    day 28

---
+++ Searching/NoOfOccurance.cs
@@ -0,0 +1,22 @@
+﻿//practise gfg link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/searching-gfg-160/problem/number-of-occurrence2259
+class Solution
+{
+    public int countFreq(int[] arr, int target)
+    {
+        // code here
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (!dict.ContainsKey(arr[i]))
+            {
+                dict.Add(arr[i], dict.GetValueOrDefault(arr[i]) + 1);
+            }
+            else
+            {
+                dict[arr[i]]++;
+            }
+        }
+
+        return dict.GetValueOrDefault(target);
+    }
+}
