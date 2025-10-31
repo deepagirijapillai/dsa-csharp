f39f29bf8cf0c2d60f9a10d7c6f2542e3109a1d4
Deepa pillai <deepapillai243@gmail.com>
2025-10-31 22:09:52 +0530

    day 27

---
+++ Sorting/MergeWithoutExtraSpace.cs
@@ -0,0 +1,22 @@
+﻿//practise gfg link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/sorting-gfg-160/problem/merge-two-sorted-arrays-1587115620
+class Solution
+{
+    public void mergeArrays(int[] a, int[] b)
+    {
+        int i = 0, j = a.Length - 1;
+
+        while (i < b.Length && j >= 0)
+        {
+            if (a[j] >= b[i])
+            {
+                int temp = a[j];
+                a[j] = b[i];
+                b[i] = temp;
+            }
+            i++; j--;
+        }
+
+        Array.Sort(a);
+        Array.Sort(b);
+    }
+}
