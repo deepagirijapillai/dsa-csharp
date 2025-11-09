dd0b3ce423eccee208f7436d4ac166ddb429f5f9
Deepa pillai <deepapillai243@gmail.com>
2025-11-09 12:59:23 +0530

    Day 30-31

---
+++ Searching/PeakElement.cs
@@ -0,0 +1,22 @@
+﻿//practise gfg link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/searching-gfg-160/problem/peak-element
+class Solution
+{
+    public int peakElement(int[] arr)
+    {
+        int low = 0, high = arr.Length - 1;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] < arr[mid + 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+}
